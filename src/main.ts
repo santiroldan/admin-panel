8be4042628c32ab214ab167741841437cbe2a063
Santi Roldán <supersantiprogrammer@gmail.com>
2022-10-12 21:28:41 +0200

    standalone components and add angular styles

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { routes } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+if (environment.production) enableProdMode();
+  
+bootstrapApplication(
+  AppComponent,
+  {providers: [importProvidersFrom(
+    BrowserAnimationsModule,
+    BrowserModule,
+    RouterModule.forRoot(routes), BrowserAnimationsModule
+    )]}
+);
