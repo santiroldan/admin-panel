fee6f331a59db6ac6f4fe6e57af556210025ec6b
Santi Roldán <supersantiprogrammer@gmail.com>
2022-10-13 19:37:02 +0200

    Add ngx-charts example

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -9,6 +9,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
+import { BaseChartDirective, NgChartsModule } from 'ng2-charts';
+import { Chart, ChartData, ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
+import { default as Annotation } from 'chartjs-plugin-annotation';
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-template',
@@ -17,25 +21,166 @@ import { MatCardModule } from '@angular/material/card';
   standalone: true,
   imports: [CommonModule, FormsModule,
     HttpClientModule, MatNativeDateModule, ReactiveFormsModule, MatSidenavModule, MatToolbarModule,
-    MatIconModule, MatListModule, MatCardModule]
+    MatIconModule, MatListModule, MatCardModule, NgChartsModule]
 })
 export class TemplateComponent {
 
   mobileQuery: MediaQueryList;
 
-  fillerNav = ['Dashboard', 'Users', 'Settings'];
+  fillerNav = [
+    { icon: 'dashboard', text: 'Dashboard' },
+    { icon: 'person', text: 'Users' },
+    { icon: 'settings', text: 'Settings' }
+  ];
 
   isMenuOpened = false;
-  
+
   private _mobileQueryListener: () => void;
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      },
+      {
+        data: [28, 48, 40, 19, 86, 27, 90],
+        label: 'Series B',
+        backgroundColor: 'rgba(77,83,96,0.2)',
+        borderColor: 'rgba(77,83,96,1)',
+        pointBackgroundColor: 'rgba(77,83,96,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(77,83,96,1)',
+        fill: 'origin',
+      },
+      {
+        data: [180, 480, 770, 90, 1000, 270, 400],
+        label: 'Series C',
+        yAxisID: 'y-axis-1',
+        backgroundColor: 'rgba(255,0,0,0.3)',
+        borderColor: 'red',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+  };
+
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      line: {
+        tension: 0.5
+      }
+    },
+    scales: {
+      x: {},
+      'y-axis-0':
+      {
+        position: 'left',
+      },
+      'y-axis-1': {
+        position: 'right',
+        grid: {
+          color: 'rgba(255,0,0,0.3)',
+        },
+        ticks: {
+          color: 'red'
+        }
+      }
+    },
+    plugins: {
+      legend: { display: true },
+      annotation: {
+        annotations: [
+          {
+            type: 'line',
+            scaleID: 'x',
+            value: 'March',
+            borderColor: 'orange',
+            borderWidth: 2,
+            label: {
+              display: true,
+              position: 'center',
+              color: 'orange',
+              content: 'LineAnno',
+              font: {
+                weight: 'bold'
+              }
+            }
+          },
+        ],
+      }
+    }
+  };
+
+  public lineChartType: ChartType = 'line';
+
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [350, 450, 100] },
+      { data: [50, 150, 120] },
+      { data: [250, 130, 70] }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public radarChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+  public radarChartLabels: string[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+
+  public radarChartData: ChartData<'radar'> = {
+    labels: this.radarChartLabels,
+    datasets: [
+      { data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B' }
+    ]
+  };
+  public radarChartType: ChartType = 'radar';
+
+  public polarAreaChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
+  public polarAreaChartData: ChartData<'polarArea'> = {
+    labels: this.polarAreaChartLabels,
+    datasets: [{
+      data: [300, 500, 100, 40, 120],
+      label: 'Series 1'
+    }]
+  };
+  public polarAreaLegend = true;
+
+  public polarAreaChartType: ChartType = 'polarArea';
+
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+    Chart.register(Annotation)
+
   }
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
   }
 }
